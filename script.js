e1d74b5aaf773909e7404fbd24abb72a6aa8a860
Umesh S <95226459+umeshsaravanan@users.noreply.github.com>
2023-07-09 15:37:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
+    var image = document.getElementById('myImage');
+    if (image.src.endsWith('light.png')) {
+        image.src = 'images/dark.png';
+        image.alt = 'dark';
+        image.style.height = '25px';
+        image.style.width = '25px';
+        image.style.marginTop = '0px';
+    } else {
+        image.src = 'images/light.png';
+        image.alt = 'light';
+        image.style.height = '35px';
+        image.style.width = '35px';
+        image.style.marginTop = '6px';
+    }
+}
